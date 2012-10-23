36287cbd525f816bd807607b0369a72e2c704700
Jakob Ovrum <jakobovrum@gmail.com>
2012-10-23 19:11:27 +0900

    Add more commands, saving of assigned voices

--- hcspeech/hcspeech.d
+++ hcspeech/hcspeech.d
@@ -6,7 +6,14 @@ import speech.synthesis;
 
 import std.algorithm;
 import std.array;
+import std.conv;
+import std.path;
 import std.random;
+import std.range;
+import std.string;
+
+import file = std.file;
+import io = std.stdio;
 
 Synthesizer tts;
 Voice[] allVoices;
@@ -23,11 +30,28 @@ struct ChannelInfo
 	}
 }
 
+auto findVoices(const(char)[] voiceSpecifier)
+{
+	voiceSpecifier = voiceSpecifier.toLower();
+
+	return allVoices.filter!(voice => toLower(voice.name).canFind(voiceSpecifier))();
+}
+
+Voice* voiceByName(in char[] name)
+{
+	auto voiceIndex = allVoices.countUntil!((voice, name) => voice.name == name)(name);
+	return voiceIndex == -1? null : &allVoices[voiceIndex];
+}
+
 ChannelInfo[] ttsChannels;
+Voice[string] specifiedUserVoices;
 Voice[string] userVoices;
 
 private Voice getUserVoice(ChannelInfo channel, in char[] nick)
 {
+	if(auto voice = nick in specifiedUserVoices)
+		return *voice;
+
 	if(auto voice = nick in userVoices)
 		return *voice;
 
@@ -53,6 +77,8 @@ private Voice getUserVoice(ChannelInfo channel, in char[] nick)
 	return voice;
 }
 
+immutable ttsUsage = "Usage: TTS, toggle Text To Speech for the current channel";
+
 EatMode ttsCommand(in char[][] words, in char[][] words_eol)
 {
 	auto channel = getInfo("channel");
@@ -70,11 +96,112 @@ EatMode ttsCommand(in char[][] words, in char[][] words_eol)
 	return EatMode.all;
 }
 
+immutable voiceListUsage = "Usage: VOICELIST, list available Text To Speech voices";
+
 EatMode voiceListCommand(in char[][] words, in char[][] words_eol)
 {
 	foreach(i, voice; allVoices)
 	{
 		writefln("#%s: %s", i + 1, voice.name);
+	}
+	return EatMode.all;
+}
+
+immutable assignVoiceUsage = "Usage: ASSIGNVOICE <nick> <voice name>, " ~
+                             "assign a specific voice to a user by voice name.";
+
+EatMode assignVoiceCommand(in char[][] words, in char[][] words_eol)
+{
+	if(words.length < 3)
+	{
+		writefln(assignVoiceUsage);
+		return EatMode.all;
+	}
+
+	auto nick = words[1];
+	auto voiceSpecifier = words_eol[2];
+		
+	auto voices = findVoices(voiceSpecifier);
+	if(voices.empty)
+	{
+		writefln(`No voice found for specifier "%s". Use the VOICELIST command to list available voices.`, voiceSpecifier);
+		return EatMode.all;
+	}
+
+	auto firstVoice = voices.front;
+	voices.popFront();
+
+	if(voices.empty) // Only one voice found
+	{
+		specifiedUserVoices[nick.idup] = firstVoice;
+		writefln("Assigned voice to %s: %s", nick, firstVoice.name);
+	}
+	else // Ambiguous search
+	{
+		writefln("Specified name matches multiple voices. Which did you mean?");
+		auto app = appender!string();
+			
+		(&firstVoice)[0 .. 1].chain(voices)
+			.map!(voice => voice.name)()
+			.joiner(", ")
+			.copy(app);
+
+		writefln(app.data);
+	}
+
+	return EatMode.all;
+}
+
+immutable clearVoiceUsage = "Usage: CLEARVOICE <nick>, unassign any previously " ~
+                            "assigned voice for the specified user.";
+
+EatMode clearVoiceCommand(in char[][] words, in char[][] words_eol)
+{
+	if(words.length < 2)
+	{
+		writefln(clearVoiceUsage);
+	}
+	else
+	{
+		auto nick = words[1];
+		if(nick in specifiedUserVoices)
+		{
+			specifiedUserVoices.remove(nick.idup); // Ew, idup :(
+			writefln("Voice cleared for user %s.", nick);
+		}
+		else
+		{
+			writefln("User %s doesn't have an assigned voice.", nick);
+		}
+	}
+	return EatMode.all;
+}
+
+immutable assignedVoicesUsage = "Usage: ASSIGNEDVOICES [nick filter], list assigned voices.";
+
+EatMode assignedVoicesCommand(in char[][] words, in char[][] words_eol)
+{
+	if(words.length < 2)
+	{
+		if(specifiedUserVoices.length == 0)
+			writefln("There are no assigned voices.");
+		else
+		{
+			foreach(nick, voice; specifiedUserVoices)
+				writefln("%s: %s", nick, voice.name);
+		}
+	}
+	else
+	{
+		auto nickFilter = toLower(words[1]);
+		auto matches = specifiedUserVoices.keys().filter!(nick => toLower(nick).canFind(nickFilter))();
+		if(matches.empty)
+			writefln(`Found no assigned voices with the search "%s".`, words[1]);
+		else
+		{
+			foreach(nick; matches)
+				writefln("%s: %s", nick, specifiedUserVoices[nick].name);
+		}
 	}
 	return EatMode.all;
 }
@@ -101,21 +228,98 @@ EatMode onMessage(in char[][] words, in char[][] words_eol)
 	return EatMode.none;
 }
 
+string configFilePath()
+{
+	return buildPath(getInfo("xchatdir"), "hcspeech.conf");
+}
+
+PluginInfo pluginInfo;
+
+void loadSettings()
+{
+	auto path = configFilePath();
+	if(!file.exists(path))
+		return;
+
+	auto file = io.File(path, "r");
+
+	string nick;
+	
+	foreach(line; file.byLine())
+	{
+		auto stripped = line.strip();
+		if(stripped.empty)
+			continue;
+
+		auto key = stripped.munch("^=").strip();
+
+		stripped.popFront();
+		auto value = stripped.strip();
+
+		switch(key)
+		{
+			case "nick":
+				nick = value.idup;
+				break;
+			case "voice":
+				assert(nick !is null);
+
+				if(auto voice = voiceByName(value))
+					specifiedUserVoices[nick] = *voice;
+				else
+					writefln(`No voice found for "%s", removing entry for user %s.`, value, nick);
+				break;
+			default:
+		}
+	}
+}
+
+void saveSettings()
+{
+	auto file = io.File(configFilePath(), "w");
+
+	void writeKey(in char[] key, in char[] value)
+	{
+		file.writefln("%s = %s", key, value);
+	}
+
+	writeKey("plugin_version", pluginInfo.version_);
+	file.writeln();
+
+	foreach(nick, voice; specifiedUserVoices)
+	{
+		writeKey("nick", nick);
+		writeKey("voice", voice.name);
+		file.writeln();
+	}
+}
+
 void init(ref PluginInfo info)
 {
 	info.name = "hcspeech";
 	info.description = "Text To Speech";
 	info.version_ = "0.1";
+	pluginInfo = info;
 
 	tts = Synthesizer.create();
 	allVoices = voiceList().array();
 
-	hookCommand("tts", &ttsCommand, "Usage: TTS, toggle Text To Speech for the current channel");
-	hookCommand("voicelist", &voiceListCommand, "Usage: VOICELIST, list installed Text To Speech voices");
+	loadSettings();
+
+	hookCommand("tts", &ttsCommand, ttsUsage);
+	hookCommand("voicelist", &voiceListCommand, voiceListUsage);
+	hookCommand("assignvoice", &assignVoiceCommand, assignVoiceUsage);
+	hookCommand("clearvoice", &clearVoiceCommand, clearVoiceUsage);
+	hookCommand("assignedvoices", &assignedVoicesCommand, assignedVoicesUsage);
 
 	hookServer("PRIVMSG", &onMessage);
 
 	writefln("Text To Speech plugin (hcspeech %s) successfully loaded", info.version_);
 }
 
-mixin(XchatPlugin!init);
+void shutdown()
+{
+	saveSettings();
+}
+
+mixin(XchatPlugin!(init, shutdown));
